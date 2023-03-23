be6de132ac6cf50ad38d883d1b344533a7aae2cf
Tsvetelin Petrov <42267885+tsvetelinpetrov@users.noreply.github.com>
2023-03-23 11:52:28 +0200

    Init migration created

---
+++ MyTU_api/MyTU_api.Entities/RoomPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTU_api.Entities
+{
+    public class RoomPoint
+    {
+        public int Id { get; set; }
+        public double MLongitude { get; set; }
+        public double MLatitude { get; set; }
+        public Room Room { get; set; }
+    }
+}
