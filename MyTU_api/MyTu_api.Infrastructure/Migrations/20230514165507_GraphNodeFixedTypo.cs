54a28e37f7cc5e5e417dd8c79ab6c987bb8e8a10
Tsvetelin Petrov <42267885+tsvetelinpetrov@users.noreply.github.com>
2023-05-14 23:45:59 +0300

    Create 20230514165507_GraphNodeFixedTypo.cs

---
+++ MyTU_api/MyTu_api.Infrastructure/Migrations/20230514165507_GraphNodeFixedTypo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyTU_api.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class GraphNodeFixedTypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Lattitude",
+                table: "GraphNodes",
+                newName: "Latitude");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Latitude",
+                table: "GraphNodes",
+                newName: "Lattitude");
+        }
+    }
+}
